23a965d082a96e6548a4785248aa04eda5101356
rdhaniyusuf <rahmadhani2202@gmail.com>
2019-12-27 01:59:39 +0700

    Sukses Login dan daftar

--- app/src/main/java/com/example/mobile/portaleventamikom/LoginActivity.java
+++ app/src/main/java/com/example/mobile/portaleventamikom/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.mobile.portaleventamikom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.mobile.portaleventamikom.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LoginActivity extends AppCompatActivity {
-
+    //    Deklasrasi
+    FirebaseDatabase database;
+    DatabaseReference dbref;
+    EditText edtxtUsername, edtxtPassword;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +34,54 @@ public class LoginActivity extends AppCompatActivity {
         Button btnMasuk = (Button)findViewById(R.id.btnLogin);
         Button btnDaftar = (Button)findViewById(R.id.btnDaftar);
 
+        database = FirebaseDatabase.getInstance();
+        dbref = database.getReference("User");
+
+        edtxtUsername = (EditText)findViewById(R.id.edtxtUsername);
+        edtxtPassword = (EditText)findViewById(R.id.edtxtPassword);
+
+        user =  new User();
+
         btnMasuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent masukIntent = new Intent(LoginActivity.this, MenuActivity.class);
-                startActivity(masukIntent);
+                prosesMasuk(
+                        edtxtUsername.getText().toString(),
+                        edtxtPassword.getText().toString()
+                );
             }
         });
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent daftarIntent =  new Intent(LoginActivity.this, RegistrasiActivity.class);
-                startActivity(daftarIntent);
+
             }
         });
 
 
+    }
+
+    private void prosesMasuk(final String pengguna, final String sandi){
+        dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(pengguna).exists()){
+                    User login = dataSnapshot.child(pengguna).getValue(User.class);
+                    if(login.getSandi().equals(sandi)){
+                        Toast.makeText(LoginActivity.this, "Sukses Login",Toast.LENGTH_SHORT).show();
+                        Intent daftarIntent =  new Intent(LoginActivity.this, MenuActivity.class);
+                        startActivity(daftarIntent);
+                    } else
+                        Toast.makeText(LoginActivity.this,"Sandi Salah",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(LoginActivity.this, "Username Salah",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(LoginActivity.this,"Jaringan Bermasalah", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/mobile/portaleventamikom/RegistrasiActivity.java
+++ app/src/main/java/com/example/mobile/portaleventamikom/RegistrasiActivity.java
@@ -1,24 +1,79 @@
 package com.example.mobile.portaleventamikom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.mobile.portaleventamikom.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegistrasiActivity extends AppCompatActivity {
 
+//    Deklasrasi
+    FirebaseDatabase database;
+    DatabaseReference dbref;
+    EditText regName,regNIM,regJurusan,regEmail, regPengguna, regSandi;
+
+    Button btnReg;
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try{this.getSupportActionBar().hide();}catch(NullPointerException e){}
         setContentView(R.layout.activity_registrasi_menu);
 
+        database = FirebaseDatabase.getInstance();
+        dbref = database.getReference("User");
+
+        regName = (EditText)findViewById(R.id.reg_name);
+        regNIM =  (EditText)findViewById(R.id.reg_nim);
+        regJurusan = (EditText)findViewById(R.id.reg_jurusan);
+        regEmail = (EditText)findViewById(R.id.reg_email);
+        regPengguna =(EditText)findViewById(R.id.reg_pengguna);
+        regSandi = (EditText) findViewById(R.id.reg_sandi);
+
+        user = new User();
+
         Button btnReg = (Button)findViewById(R.id.btnReg);
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                user.setNama(regName.getText().toString());
+                user.setNim(regNIM.getText().toString());
+                user.setJurusan(regJurusan.getText().toString());
+                user.setEmail(regEmail.getText().toString());
+                user.setPengguna(regPengguna.getText().toString());
+                user.setSandi(regPengguna.getText().toString());
+
+                dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getPengguna().toString()).exists()){
+                            Toast.makeText(RegistrasiActivity.this,"Pengguna sudah ada ", Toast.LENGTH_SHORT).show();
+                        } else {
+                            dbref.child(user.getPengguna().toString()).setValue(user);
+                            Toast.makeText(RegistrasiActivity.this,"Anda Sukses Daftar", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
                 Intent regIntent = new Intent(RegistrasiActivity.this, LoginActivity.class);
                 startActivity(regIntent);
             }

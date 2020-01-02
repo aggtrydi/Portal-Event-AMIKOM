154eeabd4853c965c48302adbe839f116cd62742
rdhaniyusuf <rahmadhani2202@gmail.com>
2020-01-02 17:27:58 +0700

    tampil user di mainactivity

--- app/src/main/java/com/example/mobile/portaleventamikom/View/MenuActivity.java
+++ app/src/main/java/com/example/mobile/portaleventamikom/View/MenuActivity.java
@@ -5,18 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.os.storage.StorageManager;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mobile.portaleventamikom.R;
 import com.example.mobile.portaleventamikom.fragment.DashboardFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import static com.google.firebase.database.FirebaseDatabase.getInstance;
 
 public class MenuActivity extends AppCompatActivity {
 
     BottomNavigationView bnvHome;
     DashboardFragment dashboardFragment;
+
+    FirebaseAuth uAuth;
+    FirebaseUser user;
+    FirebaseDatabase database;
+    DatabaseReference dbref;
+
+    StorageReference strgRef;
+
+    String strgPath = "User_Profil_Cover_image/";
+
+    ImageView imgViewUser;
+    TextView txtViewUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +56,45 @@ public class MenuActivity extends AppCompatActivity {
 
         dashboardFragment = new DashboardFragment();
 
+        uAuth = FirebaseAuth.getInstance();
+        user = uAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        dbref = database.getReference("user");
+        strgRef = FirebaseStorage.getInstance().getReference();
+
+
+        imgViewUser = findViewById(R.id.imgViewUser);
+        txtViewUser = findViewById(R.id.txtViewUser);
+
         changeFragment(dashboardFragment);
         bnvHome =  (BottomNavigationView) findViewById(R.id.bnvMain);
+
+        Query query = dbref.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    String nama = "" + ds.child("nama").getValue();
+                    String nim = "" + ds.child("nim").getValue();
+                    String jurusan = ""+ds.child("jurusan").getValue();
+                    String image = ""+ds.child("image").getValue();
+
+                    txtViewUser.setText(nama);
+                    try {
+                        Picasso.get().load(image).into(imgViewUser);
+                    } catch (Exception e){
+                        Picasso.get().load(R.drawable.logo).into(imgViewUser);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         bnvHome.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override

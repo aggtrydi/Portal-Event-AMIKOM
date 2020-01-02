dc1be6bc25360097959656c0949cb0ae0845659c
rdhaniyusuf <rahmadhani2202@gmail.com>
2020-01-03 04:55:39 +0700

    Postingan

--- app/src/main/java/com/example/mobile/portaleventamikom/View/PopUpEventActivity.java
+++ app/src/main/java/com/example/mobile/portaleventamikom/View/PopUpEventActivity.java
@@ -1,15 +1,35 @@
 package com.example.mobile.portaleventamikom.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
+import com.example.mobile.portaleventamikom.Adapter.EventAdapter;
+import com.example.mobile.portaleventamikom.Model.ModelPostingan;
 import com.example.mobile.portaleventamikom.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PopUpEventActivity extends AppCompatActivity {
+
+    FirebaseAuth uAuth;
+    LinearLayout linearLayout;
+    List<ModelPostingan> postinganList;
+    EventAdapter eventAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +50,18 @@ public class PopUpEventActivity extends AppCompatActivity {
         params.y = -20;
 
         getWindow().setAttributes(params);
+
+        uAuth = FirebaseAuth.getInstance();
+        linearLayout = findViewById(R.id.groupPopUpEvent);
+        postinganList = new ArrayList<>();
+        laodPost();
+
     }
+
+    private void laodPost() {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Postingan");
+
+
+    }
+
 }

25287a37c10c430fedf17d6c71a9c965fafdfdcb
agungtriyadi <agung.triyadi@students.amikom.ac.id>
2020-01-09 14:41:33 +0700

    splash screen done :)

--- app/src/main/java/com/example/mobile/portaleventamikom/activity/LoginActivity.java
+++ app/src/main/java/com/example/mobile/portaleventamikom/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.mobile.portaleventamikom.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         Button btnLogin = findViewById(R.id.btnLogin);
         Button btnDaftar =  findViewById(R.id.signUp_text);
 
